daf59d2532f80610297e1820a435face64499366
sleepy-bear <jew8969@gmail.com>
2024-12-09 23:00:42 +0900

    1. api  put delete post 추가 2. CORS 설정

--- src/main/java/org/slam/slam_backend/config/CustomServletConfig.java
+++ src/main/java/org/slam/slam_backend/config/CustomServletConfig.java
@@ -4,16 +4,22 @@ import lombok.extern.log4j.Log4j2;
 import org.slam.slam_backend.controller.formatter.LocalDateFormatter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @Log4j2
 public class CustomServletConfig implements WebMvcConfigurer {
-
     @Override
     public void addFormatters(FormatterRegistry registry) {
         log.info("...................... addFormatters");
         registry.addFormatter(new LocalDateFormatter());
+    }
 
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        //addMapping("/**") > 어떤 경로에 CORS 설정할 것인가? (모든경로)
+        //allowdOrigins("*") > 어디서부터 들어오는 경로를 허락할 것인가? (모든경로)
+        registry.addMapping("/**").allowedOrigins("*").maxAge(500).allowedMethods("GET","POST","PUT","DELETE","HEAD","OPTIONS");
     }
 }
--- src/main/java/org/slam/slam_backend/controller/TodoController.java
+++ src/main/java/org/slam/slam_backend/controller/TodoController.java
@@ -6,10 +6,9 @@ import org.slam.slam_backend.dto.PageRequestDTO;
 import org.slam.slam_backend.dto.PageResponseDTO;
 import org.slam.slam_backend.dto.TodoDTO;
 import org.slam.slam_backend.dto.service.TodoService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @Log4j2
@@ -30,5 +29,27 @@ public class TodoController {
 
         return todoService.getList(pageRequestDTO);
     }
+
+    @PostMapping("/")
+    public Map<String, Long> register(@RequestBody TodoDTO dto) {
+        log.info("TODODTO : " + dto);
+        Long  tno = todoService.register(dto);
+        return Map.of("TNO" , tno);
+    }
+
+    @PutMapping("/{tno}")
+    public Map<String, String> modify (@PathVariable("tno") Long tno, @RequestBody TodoDTO todoDTO) {
+        todoDTO.setTno(tno);
+        todoService.modify(todoDTO);
+        return Map.of("RESULT" , "SUCCESS");
+    }
+
+    @DeleteMapping("/{tno}")
+    public Map<String, String> remove(@PathVariable Long tno) {
+        todoService.remove(tno);
+        return Map.of("RESULT" , "SUCCESS");
+    }
+
+
 
 }
