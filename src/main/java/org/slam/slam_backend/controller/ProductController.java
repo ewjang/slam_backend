ade976ed60f5216723a71d0fba2429a9efc13c4b
sleepy-bear <jew8969@gmail.com>
2025-01-13 23:11:01 +0900

    파일 업로드 및 썸네일 처리

---
+++ src/main/java/org/slam/slam_backend/controller/ProductController.java
@@ -0,0 +1,38 @@
+package org.slam.slam_backend.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.slam.slam_backend.dto.ProductDTO;
+import org.slam.slam_backend.util.CustomFileUtil;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Map;
+import java.util.List;
+@RestController
+@Log4j2
+@RequiredArgsConstructor
+@RequestMapping(("/api/products"))
+public class ProductController {
+
+    private final CustomFileUtil fileUtil;
+
+
+    @PostMapping("/")
+    public Map<String, String> register(ProductDTO productDTO) {
+
+        log.info("register: " + productDTO);
+
+        List<MultipartFile> files = productDTO.getFiles();
+
+        List<String> uploadedFileNames = fileUtil.saveFiles(files);
+
+        productDTO.setUplaodFileNames(uploadedFileNames);
+
+        log.info(uploadedFileNames);
+
+        return Map.of("RESULT", "SUCCESS");
+    }
+}
